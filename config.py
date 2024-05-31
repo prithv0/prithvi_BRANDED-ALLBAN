badc5a9bafe0929819dffdd545cf393021c06191
prithv0 <bd946163@gmail.com>
2024-05-31 14:56:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     API_ID=29955483
     API_HASH="e9c7020d6f07cd3ced76566f6452ea0a"
     TOKEN="7339696983:AAGsUCF-tEVi_7pdNr7z0doP6Nz3tskyujs"
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "6752444356").split(" "))
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "5699973443").split(" "))
     START_IMG="https://graph.org/file/fc55cf501661789341af7.jpg"
     BOT_ID=7339696983
     BOT_USERNAME="Segoxmusicbot"
