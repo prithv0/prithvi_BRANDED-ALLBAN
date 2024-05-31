4684b20a739e0e6b6516ee106ed4c3e4656a95e2
prithv0 <bd946163@gmail.com>
2024-05-31 14:41:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config:
-    API_ID=22981583
-    API_HASH="c02ca0834d5d92ffb31fea2b50060195"
-    TOKEN="7156025226:AAFaTvFJwfkHLGcP2JZqY6GbfnUfajkQY5o"
+    API_ID=29955483
+    API_HASH="e9c7020d6f07cd3ced76566f6452ea0a"
+    TOKEN="7339696983:AAGsUCF-tEVi_7pdNr7z0doP6Nz3tskyujs"
     SUDO = list(int(i) for i in os.environ.get("SUDO", "6752444356").split(" "))
     START_IMG="https://graph.org/file/fc55cf501661789341af7.jpg"
-    BOT_ID=7156025226
-    BOT_USERNAME="SOMEONE47BOT"
+    BOT_ID=7339696983
+    BOT_USERNAME="Segoxmusicbot"
     BOT_NAME="SOMEONE47BOT"
