3663f7acf5da4cc983706a551f5b285e89e4ba5a
prithv0 <bd946163@gmail.com>
2024-06-01 10:52:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config:
-    API_ID=22981583
-    API_HASH="c02ca0834d5d92ffb31fea2b50060195"
-    TOKEN="7156025226:AAFaTvFJwfkHLGcP2JZqY6GbfnUfajkQY5o"
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "6752444356").split(" "))
+    API_ID=22793103
+    API_HASH="05c81656fbadb9126585f4249626571c"
+    TOKEN="7004891057:AAGWrUWO2iaEsAoXuNcWhh2CqIld-3HMTSM"
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "1431026748").split(" "))
     START_IMG="https://te.legra.ph/file/735920504b44b3565242a.jpg"
-    BOT_ID=7156025226
-    BOT_USERNAME="SOMEONE47BOT"
+    BOT_ID=7004891057
+    BOT_USERNAME="shibam_test_bot"
     BOT_NAME="SOMEONE47BOT"
